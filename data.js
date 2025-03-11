2bb399825fc2fa6d1f72ea961c62f38ec434275a
danchaud-vincent <vdanchaud@gmail.com>
2025-03-11 10:52:28 +0100

    create data file

---
+++ data.js
@@ -0,0 +1,20 @@
+const menu = [
+    {
+        title: "Pizza",
+        details: "pepperoni,mushrom,mozarella",
+        image: "images/pizza.png",
+        price: 14
+    },
+    {
+        title: "Hamburger",
+        details: "beef, cheese, lettuce",
+        image: "images/burger.png",
+        price: 12
+    },
+    {
+        title: "Beer",
+        details: "grain, hops, yeast, water",
+        image: "images/beer.png",
+        price: 8
+    }
+]
